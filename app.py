431ca01fdfc4b5d9e5886c83eb4073914bc78ef3
svalluru <svalluru@redhat.com>
2024-05-29 17:28:22 -0400

    app.py changed

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from kafka import KafkaConsumer
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores.redis import Redis
+import logging
 
-app = Flask(__name__)
+# To consume kafka messages
+consumer = KafkaConsumer('customerdata',
+                         bootstrap_servers=['my-cluster-kafka-bootstrap.sri-support-tech-app.svc.cluster.local:9092'],auto_offset_reset='earliest', enable_auto_commit=False)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+redis_url = "redis://default:q07rXXlq@my-doc-headless.sri-support-tech-app.svc.cluster.local:15928"
+index_name = "customerdataidx"
+schema_name = "redis_schema.yaml"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+embeddings = HuggingFaceEmbeddings()
+
+#logging.basicConfig(level=logging.DEBUG)
+
+for message in consumer:
+    # message value and key are raw bytes !
+    print(message.value.decode("utf-8"))
+    str = message.value.decode("utf-8")
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=100,
+    length_function=len,
+    is_separator_regex=False,)
+
+    texts = text_splitter.create_documents([str])
+    rds = Redis.from_documents(texts,
+                           embeddings,
+                           redis_url=redis_url,
+                           index_name=index_name)
+    rds.write_schema("redis_schema.yaml")
+    rds.add_documents(texts)
+    #print(str)
+
+#xml += ''.join(kafkadata)
+print("before")
+#print(xml)
+#text_loader_kwargs={'autodetect_encoding': True}
+#loader = DirectoryLoader('/Users/svalluru/PycharmProjects/openllm-langchain/kafkafiles', glob="*.txt", show_progress=True, loader_cls=TextLoader, silent_errors=True, loader_kwargs=text_loader_kwargs)
+#docs = loader.load()
+#print(docs)
+
+
+
+
